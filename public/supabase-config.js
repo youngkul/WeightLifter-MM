7f82908abc4ae362f1f62eab98f830bbaf539490
young kul yang <youngkulyang@youngui-MacBookPro.local>
2025-04-30 23:07:34 +0900

    🔗 Supabase 연결 완료

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -1,6 +1,10 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// public/supabase-config.js
 
-export const supabase = createClient(
-  'https://zgbvadhrmufncknjsqfe.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYnZhZGhybXVmbmNrbmpzcWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMTQ0MzcsImV4cCI6MjA2MTU5MDQzN30.U4dus0YDHPtw2VJVK_0a9ZApdgvfwdpEcujCIt0y6BE'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabase = createClient(
+  "https://zgbvadhrmufncknjsqfe.supabase.co", // ✅ Supabase 프로젝트 URL
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYnZhZGhybXVmbmNrbmpzcWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMTQ0MzcsImV4cCI6MjA2MTU5MDQzN30.U4dus0YDHPtw2VJVK_0a9ZApdgvfwdpEcujCIt0y6BE" // ✅ anon public key
 );
+
+export { supabase };
