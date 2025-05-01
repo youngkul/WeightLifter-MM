a99e8c1e91e9eb75a2996c1590c75e91570abc08
young kul yang <youngkulyang@youngui-MacBookPro.local>
2025-05-02 00:17:33 +0900

    Fix: show superAdminPanel based

--- public/app.js
+++ public/app.js
@@ -104,9 +104,17 @@ async function login() {
 }
 
 // 로그아웃
-function logout() {
-  supabase.auth.signOut();
+async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error("로그아웃 실패:", error.message);
+  } else {
+    console.log("로그아웃 성공");
+    loginSection.classList.remove("hidden");
+    mainSection.classList.add("hidden");
+  }
 }
+
 
 // 체중 저장
 async function saveWeight() {
